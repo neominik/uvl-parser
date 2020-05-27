36e059a03ee70cef3e2b40fd2f4592c085d86c0d
Dominik Engelhardt <dominikengelhardt@ymail.com>
2020-05-27 12:24:13 +0200

    Add license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject uvl-parser "0.1.0-SNAPSHOT"
   :description "Small default parser lib for UVL"
   :url "https://github.com/neominik/uvl-parser"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0" ;TODO tbd
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [instaparse "1.4.10"]
                  [com.wjoel/clj-bean "0.2.1"]]
